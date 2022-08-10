dc2bcd67ab39d83fff297a1cb7d078f57115dc0f
Theryston <fuktodo2@gmail.com>
2022-08-10 15:03:34 -0300

    handlebars

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import { cliCommands } from '../cli';
 import { Core } from '../core';
-
-const app = express();
+import { engine } from 'express-handlebars';
 
 export async function server(path: string, port: number) {
+  const app = express();
+
+  app.engine('handlebars', engine());
+  app.set('view engine', 'handlebars');
+  app.set('views', './views');
+
   const core = new Core(path, `dist`);
   await cliCommands.build();
+
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
 
   app.get('/:templateName', async (req, res) => {
     const templateName = req.params.templateName;
@@ -19,7 +28,10 @@ export async function server(path: string, port: number) {
 
     try {
       const htmlCode = await core.render(templateName, variables);
-      res.send(htmlCode);
+
+      res.render('template', {
+        htmlCode,
+      });
     } catch (error) {
       res.status(500).send(error.message);
     }
