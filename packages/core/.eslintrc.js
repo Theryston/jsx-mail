841f3d27efe53297c5ff50752b5e5b60ea22d547
Theryston Santos <funktodo2@gmail.com>
2023-11-21 18:43:20 +0000

    feat: start new core package

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,9 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
 	root: true,
-	extends: ['@jsx-mail/eslint-config/next.js'],
+	extends: ['@jsx-mail/eslint-config/react-internal.js'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		project: true,
+		project: './tsconfig.lint.json',
 	},
 };
---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export default function core(): string {
+  console.log("Hello!")
+
+  return "Hello"
+}
