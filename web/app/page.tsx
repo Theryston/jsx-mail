2a2eddd2f222f4572834c8f0ef7a47660bf0e325
= <=>
2023-12-05 17:25:10 -0300

    Add Next UI and Framer Motion dependencies, update page layout and add dark mode support

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>ok</h1>;
+// app/page.tsx
+import { Button } from '@nextui-org/button';
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold text-center">
+        JSX Mail v2 Coming Soon
+      </h1>
+      <div className="flex justify-center mt-8">
+        <Button color="primary">Nice!</Button>
+      </div>
+    </div>
+  );
 }
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,20 +1,19 @@
 import type { Config } from 'tailwindcss'
+import { nextui } from "@nextui-org/react";
+
+const isDev = process.env.VERCEL_ENV !== 'production'
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ...[isDev ? '../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}' : "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"]
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()]
 }
 export default config
