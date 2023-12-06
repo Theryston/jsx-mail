cb9f8dbf593bec4f342063f9a07dc7d71e4254ea
= <=>
2023-12-05 22:09:11 -0300

    Fix typo in email template creation

--- web/components/pages/Home/index.tsx
+++ web/components/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import { GradientSectionBorder } from './GradientSectionBorder';
 import { Hero } from './Hero';
 import { Features } from './Features';
-// import { Letter } from "./Letter";
 
 export default function Home() {
   return (
