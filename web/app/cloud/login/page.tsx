b203f7dd9f403b8c83c5a5db8ccfe5bb07fe4c37
= <=>
2024-01-31 15:30:30 -0300

    feat: add cloud nav

---
+++ web/app/cloud/login/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@nextui-org/button';
+
+export default function Page() {
+  return (
+    <div>
+      <Button color="primary" fullWidth>
+        Click me
+      </Button>
+    </div>
+  );
+}
--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Space_Grotesk } from 'next/font/google';
+import { Providers } from './providers';
 
 const font = Space_Grotesk({ subsets: ['latin'] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={font.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
