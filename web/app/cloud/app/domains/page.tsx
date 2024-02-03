5b4c1d127706fe4a5cb56465203daaa687a5e25e
= <=>
2024-02-03 14:26:46 -0300

    Add CustomTable component to Page component

--- web/app/cloud/app/domains/page.tsx
+++ web/app/cloud/app/domains/page.tsx
@@ -1,3 +1,32 @@
+'use client';
+
+import CustomTable from '../CustomTable';
+
 export default function Page() {
-  return <div>Domains</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-5 ml-2">
+        Domains
+      </h1>
+      <CustomTable
+        items={[
+          {
+            id: '1',
+            name: 'example.com',
+            status: {
+              value: 'active',
+              label: 'Verified',
+            },
+            actions: ['Edit', 'Delete'],
+          },
+        ]}
+        onActionClick={(action, item) => {
+          alert(`${action} ${item.name}`);
+        }}
+        onNewClick={() => {
+          alert('new');
+        }}
+      />
+    </>
+  );
 }
