bab3d68301ea014019c9380340223ddff570bbdc
= <=>
2024-02-06 11:18:27 -0300

    fix: add decimal calculate

--- api/src/utils/contants.ts
+++ api/src/utils/contants.ts
@@ -1,3 +1,5 @@
-export const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+export const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
 export const MONEY_SCALE = 1000000;
+export const CURRENCY = 'USD';
 export const FREE_BALANCE = 1 * MONEY_SCALE; // $1.00
+export const STORAGE_GB_PRICE = 0.05 * MONEY_SCALE; // $0.05
--- api/src/utils/friendly-money.ts
+++ api/src/utils/friendly-money.ts
@@ -1,5 +1,12 @@
-import { MONEY_SCALE } from "./contants";
+import { CURRENCY, MONEY_SCALE } from "./contants";
 
 export default function friendlyMoney(amount: number) {
-	return (amount / MONEY_SCALE).toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+	const usd = amount / MONEY_SCALE;
+	const decimals = Math.max(Math.abs(Math.min(Math.floor(Math.log10(usd)), 0)), 2);
+
+	return usd.toLocaleString('en-US', {
+		style: 'currency',
+		currency: CURRENCY,
+		minimumFractionDigits: decimals
+	})
 }
