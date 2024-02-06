15d899ed10b2eb80a99e71b0451bdbf9ba6bea23
= <=>
2024-02-06 16:58:58 -0300

    fix: add disable to no file selected

--- web/app/cloud/app/files/UploadFileModal.tsx
+++ web/app/cloud/app/files/UploadFileModal.tsx
@@ -89,6 +89,7 @@ export function UploadFileModal({
                 isLoading={isLoading}
                 fullWidth
                 color="primary"
+                disabled={file === null}
               >
                 Upload
               </Button>
