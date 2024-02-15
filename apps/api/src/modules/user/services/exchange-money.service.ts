e2837feaec305c6c8dbfcb85ce6e74fcbbd54891
Theryston Santos <devtheryston@gmail.com>
2024-02-15 14:47:39 -0300

    fix: use fetch for get exchange

--- apps/api/src/modules/user/services/exchange-money.service.ts
+++ apps/api/src/modules/user/services/exchange-money.service.ts
@@ -1,24 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { ExchangeMoneyDto } from '../user.dto';
 
 @Injectable()
 export class ExchangeMoneyService {
-  private client: AxiosInstance;
-  constructor() {
-    this.client = axios.create({
-      baseURL: `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API_KEY}`,
-    });
-  }
-
   async execute({
     amount,
     baseCurrency,
     currency,
   }: ExchangeMoneyDto): Promise<number> {
-    const { data } = await this.client.get(
-      `/pair/${baseCurrency}/${currency}/${amount}`,
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API_KEY}/pair/${baseCurrency}/${currency}/${amount}`,
     );
+
+    const data = await response.json();
 
     return data.conversion_result;
   }
