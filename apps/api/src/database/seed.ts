973fb8911bc3397a2e73928392d403b586517203
Theryston <devtheryston@gmail.com>
2024-02-17 20:11:10 -0300

    fix: import dot env

--- apps/api/src/database/seed.ts
+++ apps/api/src/database/seed.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import data from './data';
 
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exceptions.filter';
