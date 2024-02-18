1f9cad084fc7e78e45b020a22f3edb0a4cfa1a72
Theryston <devtheryston@gmail.com>
2024-02-18 19:38:41 -0300

    fix: remove all cache

--- apps/edge-api/src/index.ts
+++ apps/edge-api/src/index.ts
@@ -2,44 +2,23 @@ import { OpenAPIRouter } from "@cloudflare/itty-router-openapi";
 import { EdgeExample } from "endpoints/EdgeExample";
 import { scheduled } from "scheduled";
 
-const ONE_HOUR = 60 * 60 * 1000;
-
 export const router = OpenAPIRouter();
 
 router.get("/edge", EdgeExample);
 
-router.all("*", async (request: Request, env: Env, ctx: ExecutionContext) => {
+router.all("*", (request: Request, env: Env) => {
 	const url = new URL(request.url);
 	const path = url.pathname;
 	const searchParams = url.searchParams.toString();
-
-	const cache = caches.default;
-	const cacheKey = new Request(url.toString() + searchParams + request.method + request.headers.get('Authorization'));
-
-	let response: any = await cache.match(cacheKey);
-
-	if (!response) {
-		console.log('no cache')
-	} else {
-		console.log('cache hit')
-	}
 
 	const apiUrl = `${env.BASE_API_URL}${path}${searchParams ? `?${searchParams}` : ''}`;
 
 	console.log(`[PROXY] ${request.method} ${apiUrl}`);
 
-	const newRequest = new Request(request);
-
-	response = await fetch(apiUrl, newRequest)
-	response = new Response(response.body, response);
-	response.headers.append("Cache-Control", "s-maxage=1000000");
-
-	ctx.waitUntil(cache.put(cacheKey, response.clone()));
-
-	return response;
+	return fetch(apiUrl, request)
 });
 
 export default {
 	fetch: router.handle,
 	scheduled
-} as ExportedHandler<Env>;
+} as ExportedHandler;
