eefc3bf14a7aebf4fcdab05d16c657381dd03cc6
Theryston <devtheryston@gmail.com>
2024-02-18 19:30:18 -0300

    fix: use cache API

--- apps/edge-api/src/index.ts
+++ apps/edge-api/src/index.ts
@@ -8,10 +8,21 @@ export const router = OpenAPIRouter();
 
 router.get("/edge", EdgeExample);
 
-router.all("*", async (request: Request, env: Env) => {
+router.all("*", async (request: Request, env: Env, ctx: ExecutionContext) => {
 	const url = new URL(request.url);
 	const path = url.pathname;
 	const searchParams = url.searchParams.toString();
+
+	const cache = caches.default;
+	const cacheKey = new Request(url.toString(), request);
+
+	let response: any = await cache.match(cacheKey);
+
+	if (!response) {
+		console.log('no cache')
+	} else {
+		console.log('cache hit')
+	}
 
 	const apiUrl = `${env.BASE_API_URL}${path}${searchParams ? `?${searchParams}` : ''}`;
 
@@ -19,7 +30,13 @@ router.all("*", async (request: Request, env: Env) => {
 
 	const newRequest = new Request(request);
 
-	return await fetch(apiUrl, newRequest)
+	response = await fetch(apiUrl, newRequest)
+	response = new Response(response.body, response);
+	response.headers.append("Cache-Control", "s-maxage=1000000");
+
+	ctx.waitUntil(cache.put(cacheKey, response.clone()));
+
+	return response;
 });
 
 export default {
