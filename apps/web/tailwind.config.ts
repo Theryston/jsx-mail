c96c59afc300e79712dbf2e04fc82af7f7936022
Theryston <devtheryston@gmail.com>
2024-02-17 21:06:11 -0300

    fix: install them

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
