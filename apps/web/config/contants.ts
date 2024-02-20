a8deb91219a17849d20cc35da0511ca49d5ccc01
Theryston <devtheryston@gmail.com>
2024-02-20 09:50:56 -0300

    fix: remove not used dependencies

--- apps/web/config/contants.ts
+++
@@ -1,9 +0,0 @@
-export const MAX_EMAIL_SENDS_PER_HOUR = 100;
-export const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
-export const MAX_IMAGES_PER_IP = 100;
-export const ALLOWED_IMAGE_MIMETYPES = [
-  'image/png',
-  'image/jpg',
-  'image/jpeg',
-  'image/gif',
-];
