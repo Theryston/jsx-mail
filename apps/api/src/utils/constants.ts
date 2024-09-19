d3638bdaefb6010135cb623f944e490fd6a817d1
Theryston <devtheryston@gmail.com>
2024-09-19 11:09:29 -0300

    fix: update price per GB

--- apps/api/src/utils/constants.ts
+++ apps/api/src/utils/constants.ts
@@ -4,5 +4,5 @@ export const GATEWAY_SCALE = 100;
 export const CURRENCY = 'USD';
 export const FREE_BALANCE = 1 * MONEY_SCALE; // $1.00
 export const MINIMUM_ADD_BALANCE = 1 * MONEY_SCALE; // $1.00
-export const STORAGE_GB_PRICE = 0.02 * MONEY_SCALE; // $0.02
+export const STORAGE_GB_PRICE = 0.025 * MONEY_SCALE; // $0.025
 export const PRICE_PER_MESSAGE = 0.0001 * MONEY_SCALE; // $0.0001
