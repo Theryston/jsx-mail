13f53095b65e0529d850f4bb7aa31c1e02e3d3f5
Theryston <devtheryston@gmail.com>
2024-09-26 16:50:58 -0300

    fix: pricing page

--- apps/web/components/Pricing.tsx
+++ apps/web/components/Pricing.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { Card, CardBody, CardHeader, Slider } from '@nextui-org/react';
+import { Slider, Spinner } from '@nextui-org/react';
 import { useCallback, useEffect, useState } from 'react';
-import SectionsList from '../app/cloud/app/SectionsList';
-import SectionItem from '../app/cloud/app/SectionItem';
-import { usePathname } from 'next/navigation';
 import axios from '@/app/utils/axios';
 import Table from '@/app/cloud/app/Table';
+import { toast } from 'react-toastify';
 
 type Price = {
-  title: string;
-  unit: string;
+  unit: number;
   unitName: string;
-  amount: number;
+  description: string;
   step: number;
   maxValue: number;
   price: number;
@@ -29,6 +26,7 @@ type SelectedValues = {
 };
 
 export default function Pricing() {
+  const [isLoading, setIsLoading] = useState(true);
   const [selectedValues, setSelectedValues] = useState<SelectedValues>({});
   const [pricing, setPricing] = useState<Price[]>([]);
   const [moneyScale, setMoneyScale] = useState(0);
@@ -40,13 +38,16 @@ export default function Pricing() {
 
   const fetchPricing = useCallback(async () => {
     try {
+      setIsLoading(true);
       const { data } = await axios.get('/user/price');
 
       setPricing(data.pricing);
       setMoneyScale(data.moneyScale);
       setFreeBalance(data.freeBalance);
     } catch (error) {
-      console.log(error);
+      toast.error('Failed to fetch pricing');
+    } finally {
+      setIsLoading(false);
     }
   }, []);
 
@@ -60,11 +61,11 @@ export default function Pricing() {
     let totalHere = 0;
 
     for (const value of values) {
-      totalHere += (value.value / value.price.amount) * value.price.price;
+      totalHere += (value.value / value.price.unit) * value.price.price;
     }
 
     const emails =
-      values.find((value) => value.price.title === 'Emails')?.value || 0;
+      values.find((value) => value.price.unitName === 'emails')?.value || 0;
 
     const totalOthers =
       emails < 100000
@@ -84,69 +85,87 @@ export default function Pricing() {
   }, [selectedValues, moneyScale]);
 
   return (
-    <div className="mt-6 flex flex-col gap-10">
-      <p>
-        Our pricing system is very straightforward: You add a balance, and we
-        automatically deduct from that balance. Every month we add{' '}
-        {(freeBalance / moneyScale).toLocaleString('en-US', {
-          style: 'currency',
-          currency: 'USD',
-        })}{' '}
-        to your balance for free!
-      </p>
-
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 items-end">
-        <div className="flex flex-col gap-3">
-          {pricing.map((price) => (
-            <Slider
-              size="sm"
-              key={price.title}
-              label={price.title}
-              step={price.step}
-              maxValue={price.maxValue}
-              value={selectedValues[price.title]?.value || 0}
-              onChange={(value) =>
-                setSelectedValues((old) => ({
-                  ...old,
-                  [price.title]: { price, value: value as number },
-                }))
-              }
-            />
-          ))}
+    <>
+      {isLoading && (
+        <div className="mt-6 w-full h-52 flex justify-center items-center">
+          <Spinner />
         </div>
-        <ul className="flex flex-col gap-1">
-          <li className="flex justify-between items-center w-full">
-            <span>JSX Mail Cloud</span>
-            <span className="text-gray-500">
-              {(calculationResult.here < freeBalance
-                ? 0
-                : calculationResult.here / moneyScale
-              ).toLocaleString('en-US', {
-                style: 'currency',
-                currency: 'USD',
-              })}
-            </span>
-          </li>
-          <li className="flex justify-between items-center w-full">
-            <span>Other companies</span>
-            <span className="text-gray-500">
-              {(calculationResult.others / moneyScale).toLocaleString('en-US', {
-                style: 'currency',
-                currency: 'USD',
-              })}
-            </span>
-          </li>
-        </ul>
-      </div>
+      )}
+      {!isLoading && (
+        <div className="mt-6 flex flex-col gap-10">
+          <p>
+            <b>
+              The JSX Mail as a tool is totally free! You just pay if you want
+              to use the JSX Mail Cloud!
+            </b>
+            <br />
+            <br />
+            Our pricing system is very straightforward: You add a balance, and
+            we automatically deduct from that balance. Every month we add{' '}
+            {(freeBalance / moneyScale).toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })}{' '}
+            to your balance for free!
+          </p>
 
-      <Table
-        columns={['Title', 'Unit', 'Price']}
-        rows={pricing.map((price) => [
-          price.title,
-          `${price.unit} ${price.unitName}`,
-          `${price.friendlyAmount}`,
-        ])}
-      />
-    </div>
+          <div className="grid grid-cols-1 gap-12 md:grid-cols-2 items-end">
+            <div className="flex flex-col gap-3">
+              {pricing.map((price) => (
+                <Slider
+                  size="sm"
+                  key={price.unitName}
+                  label={price.unitName}
+                  step={price.step}
+                  maxValue={price.maxValue}
+                  value={selectedValues[price.unitName]?.value || 0}
+                  onChange={(value) =>
+                    setSelectedValues((old) => ({
+                      ...old,
+                      [price.unitName]: { price, value: value as number },
+                    }))
+                  }
+                />
+              ))}
+            </div>
+            <ul className="flex flex-col gap-1">
+              <li className="flex justify-between items-center w-full">
+                <span>JSX Mail Cloud</span>
+                <span className="text-gray-500">
+                  {(calculationResult.here < freeBalance
+                    ? 0
+                    : calculationResult.here / moneyScale
+                  ).toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                  })}
+                </span>
+              </li>
+              <li className="flex justify-between items-center w-full">
+                <span>Other companies</span>
+                <span className="text-gray-500">
+                  {(calculationResult.others / moneyScale).toLocaleString(
+                    'en-US',
+                    {
+                      style: 'currency',
+                      currency: 'USD',
+                    },
+                  )}
+                </span>
+              </li>
+            </ul>
+          </div>
+
+          <Table
+            columns={['Description', 'Unit', 'Price']}
+            rows={pricing.map((price) => [
+              price.description,
+              `${price.unit} ${price.unitName}`,
+              `${price.friendlyAmount}`,
+            ])}
+          />
+        </div>
+      )}
+    </>
   );
 }
