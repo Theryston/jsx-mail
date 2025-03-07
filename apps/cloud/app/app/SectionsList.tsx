208438cbb9f0b743eae0f67552df51a085ef3ebf
Theryston <devtheryston@gmail.com>
2025-03-07 10:38:36 -0300

    fix: new cloud

--- apps/cloud/app/app/SectionsList.tsx
+++
@@ -1,7 +0,0 @@
-export default function SectionsList({
-  children,
-}: {
-  children: React.ReactNode | React.ReactNode[];
-}) {
-  return <ul className="flex flex-col gap-10 w-full">{children}</ul>;
-}
--- apps/cloud/app/page.tsx
+++ apps/cloud/app/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation';
-
-export default function Page() {
-  redirect('/app');
+export default function Home() {
+  return (
+    <main>
+      <div>Hello world!</div>
+    </main>
+  );
 }
