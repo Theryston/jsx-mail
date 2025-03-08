1a3962c9ddbf1ab78e970c9b9ac5a6db76f8bd31
Theryston <devtheryston@gmail.com>
2025-03-08 17:20:30 -0300

    fix: import button

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -3,7 +3,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { Loader2 } from 'lucide-react';
 
-import { cn } from '../lib/utils.js';
+import { cn } from 'src/lib/utils.js';
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-[color,box-shadow,transform] duration-200 ease-in-out active:scale-95 active:transition-transform active:duration-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
