4ba4952dd66a069176175198485f3b1b07b171d9
Theryston <devtheryston@gmail.com>
2025-03-09 11:17:34 -0300

    fix: types

--- apps/institutional/app/providers.tsx
+++ apps/institutional/app/providers.tsx
@@ -5,9 +5,9 @@ import { HeroUIProvider } from '@heroui/system';
 import { useRouter } from 'next/navigation';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
+import { QueryClient } from '@tanstack/react-query';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -20,41 +20,36 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   return (
     <HeroUIProvider navigate={router.push}>
       <NextThemesProvider {...themeProps}>
-        <CacheProvider>{children}</CacheProvider>
+        <QueryClientProvider>{children}</QueryClientProvider>
       </NextThemesProvider>
     </HeroUIProvider>
   );
 }
 
-const CacheProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isClient, setIsClient] = React.useState(false);
+function QueryClientProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(
     () =>
       new QueryClient({
         defaultOptions: {
           queries: {
-            gcTime: 1000 * 60 * 60 * 24,
-            retry(failureCount, error: any) {
-              if (error.response?.status === 403) return false;
-              return failureCount < 3;
+            retry: (failureCount, error: any) => {
+              if (failureCount >= 3) {
+                return false;
+              }
+
+              if ([401, 403, 404].includes(error.status)) {
+                return false;
+              }
+
+              return true;
             },
           },
         },
       }),
   );
 
-  React.useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return (
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    );
-  }
-
   const persister = createSyncStoragePersister({
-    storage: window.localStorage,
+    storage: typeof window !== 'undefined' ? window.localStorage : null,
     serialize: (data: any) => {
       const newQueries: any[] = [];
       const queries = data.clientState.queries || [];
@@ -95,7 +90,8 @@ const CacheProvider = ({ children }: { children: React.ReactNode }) => {
       client={queryClient}
       persistOptions={{ persister }}
     >
+      {/* @ts-ignore */}
       {children}
     </PersistQueryClientProvider>
   );
-};
+}
