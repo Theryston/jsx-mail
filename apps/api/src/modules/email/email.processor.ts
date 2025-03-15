af9646096ffaa03b23cac7e8b3893b23e6a700bd
Theryston <devtheryston@gmail.com>
2025-03-15 09:33:09 -0300

    fix: ses

--- apps/api/src/modules/email/email.processor.ts
+++ apps/api/src/modules/email/email.processor.ts
@@ -9,9 +9,6 @@ import axios from 'axios';
 
 const transporter = nodemailer.createTransport({
   SES: { aws, ses: sesClient },
-  ses: {
-    ConfigurationSetName: process.env.AWS_SES_CONFIGURATION_SET as string,
-  },
 });
 
 @Processor('email')
@@ -74,6 +71,9 @@ export class EmailProcessor extends WorkerHost {
       subject,
       html,
       attachments,
+      ses: {
+        ConfigurationSetName: process.env.AWS_SES_CONFIGURATION_SET as string,
+      },
     } as any);
 
     if (messageId) {
