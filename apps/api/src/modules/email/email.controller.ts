13ac0268cb434a0ef4bdb2dce2c08184d102fc58
Theryston <devtheryston@gmail.com>
2025-03-21 12:14:46 -0300

    feat: log raw body

--- apps/api/src/modules/email/email.controller.ts
+++ apps/api/src/modules/email/email.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { EmailWebhookService } from './services/email-webhook.service';
 
 @Controller('email')
@@ -6,8 +6,9 @@ export class EmailController {
   constructor(private readonly emailWebhookService: EmailWebhookService) {}
 
   @Post('webhook')
-  async emailWebhook(@Body() data: any, @Headers() headers: any) {
-    console.log('[EMAIL_WEBHOOK] request headers: ', headers);
+  async emailWebhook(@Body() data: any, @Req() req: Request) {
+    const rawBody = await req.text();
+    console.log('[EMAIL_WEBHOOK] raw body: ', rawBody);
 
     const result = await this.emailWebhookService.execute(data);
     console.log('[EMAIL_WEBHOOK] result: ', result);
