ebd3b5d1a1d013a7d3fabf834df689d2c8ec9504
Theryston <devtheryston@gmail.com>
2025-04-08 11:33:38 -0300

    fix: get users

--- apps/cloud/app/(private)/admin/users/page.tsx
+++ apps/cloud/app/(private)/admin/users/page.tsx
@@ -27,17 +27,18 @@ export default function AdminUsers() {
   return (
     <Container header>
       <div className="flex flex-col gap-4">
-        <div className="flex items-center justify-between">
+        <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full gap-4">
           <h1 className="text-2xl">
             <span className="font-bold">Admin,</span> users
           </h1>
 
-          <Input
-            placeholder="Search users..."
-            value={searchTerm}
-            className="w-64"
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+          <div className="w-full md:w-64">
+            <Input
+              placeholder="Search users..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
         </div>
 
         {isPending ? (
