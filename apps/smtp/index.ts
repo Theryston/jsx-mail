38c9bd8019f3dbea017b24f3cf7397c68cd9fb53
Theryston <devtheryston@gmail.com>
2025-04-19 13:47:36 -0300

    feat: calling API

--- apps/smtp/index.ts
+++ apps/smtp/index.ts
@@ -1,28 +1,112 @@
 import { SMTPServer } from 'smtp-server';
 import type { SMTPServerAuthentication, SMTPServerSession } from 'smtp-server';
 import { simpleParser } from 'mailparser';
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: process.env.API_BASE_URL,
+});
 
 const server = new SMTPServer({
   secure: false,
   disabledCommands: ['STARTTLS'],
-  onAuth: (
+  onAuth: async (
     auth: SMTPServerAuthentication,
-    session: SMTPServerSession,
+    _session: SMTPServerSession,
     callback: (err: Error | null, response?: { user: string }) => void,
   ) => {
-    // TODO: Implement authentication
-    callback(null, { user: auth.username || 'anonymous' });
+    if (!auth.username) {
+      callback(new Error('Username is required'));
+      return;
+    }
+
+    if (!auth.password) {
+      callback(new Error('Password is required'));
+      return;
+    }
+
+    if (auth.username !== 'jsxmail') {
+      callback(new Error('Invalid username'));
+      return;
+    }
+    try {
+      await apiClient.get('/user/me', {
+        headers: {
+          Authorization: `Bearer ${auth.password}`,
+        },
+      });
+
+      callback(null, { user: auth.password });
+    } catch (error) {
+      callback(
+        new Error('Invalid credentials. Set the password as your API key.'),
+      );
+      return;
+    }
   },
   onData: async (stream, session, callback) => {
-    const parsed = await simpleParser(stream);
-    // TODO: Implement email sending by API
+    const apiKey = session.user;
 
-    console.log('New email received:');
-    console.log(parsed);
-    console.log('----------------------------------------');
-    callback();
+    if (!apiKey) {
+      callback(
+        new Error('Invalid credentials. Set the password as your API key.'),
+      );
+      return;
+    }
+
+    if (typeof apiKey !== 'string') {
+      callback(
+        new Error('Invalid credentials. Set the password as your API key.'),
+      );
+      return;
+    }
+
+    const parsed = await simpleParser(stream);
+
+    const toTexts = Array.isArray(parsed.to)
+      ? parsed.to.map((to) => to.text)
+      : [parsed.to?.text];
+
+    const to = toTexts.flatMap((toText) => {
+      if (!toText) {
+        return [];
+      }
+
+      return toText.split(', ');
+    });
+
+    const sender = parsed.from?.value[0]?.address;
+    const subject = parsed.subject;
+    const html = parsed.html || parsed.text;
+    const attachments =
+      parsed.attachments?.map((attachment) => ({
+        fileName: attachment.filename,
+        content: attachment.content.toString('base64'),
+      })) || [];
+
+    try {
+      await apiClient.post(
+        '/sender/send',
+        {
+          to,
+          sender,
+          subject,
+          html,
+          attachments,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        },
+      );
+
+      callback(null);
+    } catch (error: any) {
+      const errorData = error.response?.data?.message || error.message;
+      callback(new Error(errorData));
+    }
   },
-  logger: true,
 });
 
 server.listen(Number(process.env.PORT), '0.0.0.0', () => {
--- apps/smtp/test.ts
+++ apps/smtp/test.ts
@@ -1,19 +1,37 @@
 import { createTransport } from 'nodemailer';
 
-const { PORT } = process.env;
+const PORT = Number(process.argv[2]);
+const HOST = process.argv[3];
+
+const user = process.argv[4];
+const pass = process.argv[5];
+const to = process.argv[6];
+
+console.log(JSON.stringify({ PORT, HOST, user, pass, to }, null, 2));
 
 const transporter = createTransport({
-  host: 'localhost',
-  port: Number(PORT),
+  host: HOST,
+  port: PORT,
   auth: {
-    user: 'test@test.com',
-    pass: 'test',
+    user,
+    pass,
   },
 });
 
-transporter.sendMail({
-  from: 'test@test.com',
-  to: 'test@test.com',
-  subject: 'Test',
-  text: 'Hello',
-});
+transporter
+  .sendMail({
+    from: 'cloud@jsxmail.org',
+    to,
+    subject: 'Test JSX Mail SMTP',
+    html: '<p>Hello</p>',
+    attachments: [
+      {
+        filename: 'test.txt',
+        content: 'Hello',
+      },
+    ],
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => console.log(error));
